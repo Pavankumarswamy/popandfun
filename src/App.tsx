4c62c3d7cbd5249c4899da348ee73d950e4c79d8
Pavan <shesettipavankumarswamy@gmail.com>
2025-10-27 07:57:29 +0530

    Use BrowserRouter by default with GH Pages fallback; make GH Pages base only in deploy build

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { CartProvider } from "./contexts/CartContext";
 import Navbar from "./components/Navbar";
@@ -21,17 +21,23 @@ const App = () => (
         <CartProvider>
           <Toaster />
           <Sonner />
-          <HashRouter>
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Products />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/admin/login" element={<AdminLogin />} />
-              <Route path="/admin" element={<AdminDashboard />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </HashRouter>
+          {(() => {
+            const isGhPages = typeof window !== 'undefined' && window.location.hostname.endsWith('github.io');
+            const Router = isGhPages ? HashRouter : BrowserRouter;
+            return (
+              <Router>
+                <Navbar />
+                <Routes>
+                  <Route path="/" element={<Products />} />
+                  <Route path="/cart" element={<Cart />} />
+                  <Route path="/admin/login" element={<AdminLogin />} />
+                  <Route path="/admin" element={<AdminDashboard />} />
+                  {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </Router>
+            );
+          })()}
         </CartProvider>
       </AuthProvider>
     </TooltipProvider>
