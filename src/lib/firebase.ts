ce3fa1818f44c53ba29fb66616a39bcc42210ea9
Pavankumarswamy <shesettipavankumarswamy@gmail.com>
2025-10-27 08:12:59 +0530

    Remove Firebase credentials from firebase.ts
    Removed sensitive Firebase configuration details.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,10 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcM567OP8HEdR6v92xuq5t114aMnIZ7BA",
-  authDomain: "pop-and-fun.firebaseapp.com",
-  databaseURL: "https://pop-and-fun-default-rtdb.firebaseio.com",
-  projectId: "pop-and-fun",
-  storageBucket: "pop-and-fun.firebasestorage.app",
-  messagingSenderId: "845863097371",
-  appId: "1:845863097371:web:c407ac055a80fc2dee94be"
+  
+
+  firebase credentials
+  
 };
 
 const app = initializeApp(firebaseConfig);
